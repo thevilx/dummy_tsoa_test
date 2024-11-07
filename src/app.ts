f1b03e488f65e1cc76f866886e98247dc0d44b30
Mohammad Amin Mirzaei <amin.mir1380@gmail.com>
2024-11-07 14:37:57 +0330

    add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 // src/app.ts
-import express, { json, urlencoded, Response as ExResponse, Request as ExRequest } from "express";
 import { RegisterRoutes } from "../tsoa/routes";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from '../tsoa/swagger.json';
+import { ValidateError } from "tsoa";
+import express, {
+    Response,
+    Request,
+    NextFunction,
+    urlencoded,
+    json,
+} from "express";
 
 export const app = express();
 
-app.use("/docs", swaggerUi.serve, async (_req: any, res: any) => {
-    return res.send(
-        swaggerUi.generateHTML(await import("../tsoa/swagger.json"))
-    );
-});
 
 // Use body parser to read sent json payloads
 app.use(
@@ -21,6 +22,32 @@ app.use(
 app.use(json());
 
 
+app.use("/docs", swaggerUi.serve, async (_req: Request, res: Response) => {
+    return res.send(
+        swaggerUi.generateHTML(await import("../tsoa/swagger.json"))
+    );
+});
+
 RegisterRoutes(app);
 
+app.use(function errorHandler(
+    err: unknown,
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Response | void {
+    if (err instanceof ValidateError) {
+        console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+        return res.status(422).json({
+            message: "Validation Failed",
+            details: err?.fields,
+        });
+    }
+    if (err instanceof Error) {
+        return res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
 
+    next();
+});
